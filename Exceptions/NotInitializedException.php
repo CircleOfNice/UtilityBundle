66975f6ce0535a27be44bc016229acb71047a902
donjuandem <marco.sliwa@teeage-beatz.de>
2014-07-14 13:14:52 +0200

    changed docs

--- Exceptions/NotInitializedException.php
+++ Exceptions/NotInitializedException.php
@@ -1,20 +1,4 @@
 <?php
-/*
- * Ci\UtilityBundle provides a set of reusable php/symfony utilities
- * (C) 2014 TeeAge-Beatz UG
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
- */
-
 /**
  * exception should be thrown if not everything needed
  * to perform an operation is initialized in a class or
@@ -23,20 +7,21 @@
 namespace Ci\UtilityBundle\Exceptions;
 
 /**
- * 
- * @author		Marco Sliwa <marco.sliwa@teeage-beatz.de>
- * @package		Ci\UtilityBundle\Exceptions
- * @copyright	TeeAge-Beatz UG 2013-2014
+ *
+ * @package   Ci\UtilityBundle
+ * @author    Marco Sliwa <marco.sliwa@teeage-beatz.de>
+ * @copyright 2014 TeeAge-Beatz UG (haftungsbeschraenkt)
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
  * @internal
  */
 class NotInitializedException extends \Exception {
 	/**
 	 * sets the message
-	 * 
+	 *
 	 * @param	object	$message
 	 * @param	string	$method
 	 */
 	public function __construct($class, $method) {
 		$this->message = get_class($class)." is not initialized, please call '".$method."' before using this class!";
-	}	
+	}
 }
--- Exceptions/WrongTypeException.php
+++ Exceptions/WrongTypeException.php
@@ -1,20 +1,4 @@
 <?php
-/*
- * Ci\UtilityBundle provides a set of reusable php/symfony utilities
- * (C) 2014 TeeAge-Beatz UG
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
- */
-
 /**
  * thrown if an argument has the wrong type
  */
@@ -23,17 +7,18 @@ namespace Ci\UtilityBundle\Exceptions;
 use Ci\UtilityBundle\Traits\DebugPrinter;
 
 /**
- * @author		Marco Sliwa <marco.sliwa@teeage-beatz.de>
- * @package		Ci\UtilityBundle\Exceptions
- * @copyright	TeeAge-Beatz UG 2013-2014
+ * @package   Ci\UtilityBundle
+ * @author    Marco Sliwa <marco.sliwa@teeage-beatz.de>
+ * @copyright 2014 TeeAge-Beatz UG (haftungsbeschraenkt)
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
  * @internal
  */
 class WrongTypeException extends \Exception {
 	use DebugPrinter;
-	
+
 	/**
 	 * constructor
-	 * 
+	 *
 	 * @param	string	$name
 	 * @param	object	$param
 	 * @param	string	$expected
